e51712ac6815e80fa91e90af3de885091ec48700
unknown <abdullahalomar048@gmail.com>
2022-03-02 00:41:29 +0600

    api id added

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,16 @@
 const loadPhone = () => {
     const phoneSearchField = document.getElementById('search-field');
-    const phoneSearchText = phoneSearchField.value;
-    console.log(phoneSearchText);
+    const searchText = phoneSearchField.value;
+    console.log(searchText);
     // clear data
     phoneSearchField.value = '';
 
     // load data
-    if (phoneSearchText == '') {
+    if (searchText == '') {
         //code....
     } else {
         
-        const url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearchText}`;
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         fetch(url)
         .then(response => response.json())
         .then(json => displaySearchResult(json.data))
@@ -29,16 +29,42 @@ const loadPhone = () => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-              <div class="card">
+              <div class="card h-100"">
               <img src="${data.image}" class="card-img-top" alt=" ">
               <div class="card-body">
-              <h5 class="card-title">${data.brand}</h5>
-              <p class="card-text">${data.slug}</p>
+              <h4 class="card-title">${data.brand}</h4>
+              <h6 class="card-text">${data.slug}</h6>
               </div>
               </div>
             `;
             searchResult.appendChild(div);
         })
+    }
+
+    // details data
+    const loadDetails = id => {
+        const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+        fetch(url)
+        .then(response => response.json())
+        .then(json => displayMealDetail(json.data[0]))
+    
+    }
+    
+    const displayMealDetail = data => {
+        console.log(data);
+    //     const mealDetails = document.getElementById('meal-details');
+    //     mealDetails.textContent = '';
+    //     const div = document.createElement('div');
+    //     div.classList.add('card');
+    //     div.innerHTML = `
+    //     <img src="${data.image}" class="card-img-top" alt="...">
+    //     <div class="card-body">
+    //     <h5 class="card-title">${data.brand}</h5>
+    //     <p class="card-text">${data.strInstructions.slice(0, 150)}</p>
+    //     <a href="${data.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    //     </div>
+    //     `;
+    //     mealDetails.appendChild(div);
     }
 
  
