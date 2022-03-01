24ac6ebe4e1e1665ccf66bd97f61613b981db4e6
unknown <abdullahalomar048@gmail.com>
2022-03-01 23:23:42 +0600

    result show on displsy

--- js/phone.js
+++ js/phone.js
@@ -17,11 +17,25 @@ const loadPhone = () => {
     }
 }
       
-   //search result data
+   // Show result on display 
     const displaySearchResult = datas => {
         const searchResult = document.getElementById('search-result');
         searchResult.textContent = '';
         if (datas.length == 0) {
             // code.....
         }
+        datas.forEach( data => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innnerHtml = `
+              <div class="card">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+              </div>
+            `;
+            searchResult.appendChild(div);
+        });
     }
